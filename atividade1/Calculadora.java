95ad0116252c1c71e1e96a674ad31f1902435100
michelfviana <veteroca007@gmail.com>
2024-03-19 18:30:16 -0300

    Envio de atividade1 (lista1)

---
+++ atividade1/Calculadora.java
@@ -0,0 +1,35 @@
+package aula2;
+
+public class Calculadora {
+    
+    Double numA;
+    Double numB;
+
+    Double somar (){
+        return numA + numB;
+    }
+
+    Double subtrair (){
+        return numA - numB;
+    }
+
+    Double multiplicar (){
+        return numA * numB;
+    }
+
+    Double dividir (){
+        return numA / numB;
+    }
+
+   Boolean verificarNumeroPrimo(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+   }
+}
---
+++ atividade1/Main.java
@@ -0,0 +1,26 @@
+package aula2;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Calculadora calc = new Calculadora();
+        calc.numA = (double) 2;
+        calc.numB = (double) 3;
+        
+        System.out.println("Somar: " + calc.somar());
+        System.out.println("Subtrair: " + calc.subtrair());
+        System.out.println("multiplicar: " + calc.multiplicar());
+        System.out.println("dividir: " + calc.dividir());
+        System.out.println(": " + calc.dividir());
+        System.out.println("número primo:" + calc.verificarNumeroPrimo(0)); //false
+        System.out.println("número primo:" + calc.verificarNumeroPrimo(1)); //false
+        System.out.println("número primo:" + calc.verificarNumeroPrimo(2)); //true
+        System.out.println("número primo:" + calc.verificarNumeroPrimo(3)); //true
+        System.out.println("número primo:" + calc.verificarNumeroPrimo(7)); //true
+        System.out.println("número primo:" + calc.verificarNumeroPrimo(83)); //true
+        System.out.println("número primo:" + calc.verificarNumeroPrimo(100)); //false
+        System.out.println("número primo:" + calc.verificarNumeroPrimo(991)); //true
+        System.out.println("número primo:" + calc.verificarNumeroPrimo(104729)); //true
+        System.out.println("número primo:" + calc.verificarNumeroPrimo(14348907)); //false
+    }
+}
