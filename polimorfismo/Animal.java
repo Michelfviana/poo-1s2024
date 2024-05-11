ddb31dd74c5fd7e20690046a3faa57a50b11f0b0
Michel <michel.fvc@proton.me>
2024-05-11 11:02:12 -0300

    polimorfismo

---
+++ polimorfismo/Animal.java
@@ -0,0 +1,12 @@
+package polimorfismo;
+
+public class Animal {
+    public static void main(String[] args) {
+        System.out.println("Animal fazendo som!");
+
+        fazerSom()
+    }
+
+}
+
+
