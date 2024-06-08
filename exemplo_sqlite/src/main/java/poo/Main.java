12ff30428b5e8cfed623e41767d04f837a89e75a
Michel <michel.fvc@proton.me>
2024-06-08 10:52:25 -0300

    exemplo.sqlite

--- exemplo_sqlite/src/main/java/poo/Main.java
+++ exemplo_sqlite/src/main/java/poo/Main.java
@@ -1,6 +1,13 @@
 package poo;
 
-public class Main {
+import poo.dao.pessoaDAO;
+import poo.model.Pessoa;
 
-    
+public class Main {
+public static void main(String[] args) {
+    PessoaDAO dao = new PessoaDAO();
+
+    pessoa p1 = ne
+}
+
 }
